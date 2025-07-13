0a1f053dc8dbd25549791dfb797a69e4ee6ba2ef
jason-duong4509 <jasonduong09@gmail.com>
2025-07-13 15:59:05 -0600

    Added a way for the front-end to retrieve things from the database.

--- project/backend.py
+++ project/backend.py
@@ -10,14 +10,20 @@ Import the Flask framework so it can be used.
 Import request to handle requests from the front-end.
 Import jsonify so communications to the front-end are in the form of JSON.
 - JSON is used as this uses the REST standard and JSON is commonly used.
-Import render_template so that flask can grab and serve HTML files from /templates/file.html
+Import render_template so that flask can grab and serve HTML files from /templates/file.html.
+Import send_file so that flask can send user-related files (profile picture) to the front-end.
 """
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify, render_template, send_file
 
 """
 Used to connect to the database.
 """
 import psycopg2
+
+"""
+Used to treat byte data as a file.
+"""
+import io
 
 """
 Used in the context of Render. Allows this file to connect to the database via a URL.
@@ -59,7 +65,7 @@ def onViewProfile(user_id): # Takes whatever is after "/p/" and passes it as a p
     #--Setup--
     connection_to_db = psycopg2.connect(DATABASE_URL) # Connect to the DB
     db_cursor = connection_to_db.cursor() # Gets the cursor of the DB so that we can pass commands to the DB
-    username = fname = lname = bio = pfp = attachment1 = attachment2 = attachment3 = None
+    username = fname = lname = bio = None
     #---------
 
     #--Gets relevant info about user_id from user_info--
@@ -67,22 +73,18 @@ def onViewProfile(user_id): # Takes whatever is after "/p/" and passes it as a p
     user_info_table = db_cursor.fetchall() # Stores the result of the DB query (result in form of list of tuples)
     for entry in user_info_table: # user_info_table = [(UserID, Firstname, Lastname, Username, UserPassword), ...]
         if entry[0] == user_id:
-            fname = entry[1]
-            lname = entry[2]
-            username = entry[3]
+            fname = entry[1] # Grab the first name from the DB
+            lname = entry[2] # Grab the last name from the DB
+            username = entry[3] # Grab the username from the DB
             break
     #---------------------------------------------------
 
     #--Gets relevant info about user_id from profile_info--
     db_cursor.execute("SELECT * FROM profile_info") # Gets the entire table from the DB
     profile_info_table = db_cursor.fetchall() # Stores the result of the DB query (result in form of list of tuples)
-    for entry in profile_info_table: # profile_info_table = [(UserID, Bio, ProfilePictureFileName, ProfilePictureByteData, Attachment1FileName, Attachment1ByteData, Attachment2FileName, Attachment2ByteData, Attachment3FileName, Attachment3ByteData), ...]
+    for entry in profile_info_table: # profile_info_table = [(UserID, Bio, ProfilePictureFileName, ProfilePictureByteData, ProfilePictureMIMEType, Attachment1FileName, Attachment1ByteData, Attachment1MIMEType, Attachment2FileName, Attachment2ByteData, Attachment2MIMEType, Attachment3FileName, Attachment3ByteData, Attachment3MIMEType), ...]
         if entry[0] == user_id:
-            bio = entry[1]
-            pfp = entry[3]
-            attachment1 = entry[5]
-            attachment2 = entry[7]
-            attachment3 = entry[9]
+            bio = entry[1] # Grab the bio from the DB
             break
     #------------------------------------------------------
 
@@ -94,17 +96,35 @@ def onViewProfile(user_id): # Takes whatever is after "/p/" and passes it as a p
     db_cursor.close() # Teardown stuff
     connection_to_db.close() # Teardown stuff
 
-    return render_template(
-                            "profile.html", 
-                            username = username, 
-                            fname = fname, 
-                            lname = lname, 
-                            bio = bio, 
-                            pfp = pfp, 
-                            attachment1 = attachment1, 
-                            attachment2 = attachment2, 
-                            attachment3 = attachment3
-                            ) # Return profile.html to the front end with all of the placeholder values inserted into the file
+    return render_template("profile.html", username = username, fname = fname, lname = lname, bio = bio) # Return profile.html to the front end with all of the text placeholder values inserted into the file
+
+"""
+Function that returns the profile picture of a given user ID.
+Intended use alongside /profile/<user_id>.
+"""
+@webApp.route("/profile/<user_id>/get_pfp", methods = ["GET"])
+def getProfilePicture(user_id): # user_id = the user id in the URL when the request has been made
+    #--Connect to the database--
+    connection_to_db = psycopg2.connect(DATABASE_URL)
+    db_cursor = connection_to_db.cursor()
+    #---------------------------
+
+    #--Find the profile picture for user_id and return it--
+    db_cursor.execute("SELECT UserID, ProfilePictureFileName, ProfilePictureByteData, ProfilePictureMIMEType FROM profile_info") # Gets pfp info from database
+    results_table = db_cursor.fetchall()
+    for entry in results_table: # entry = (UserID, ProfilePictureFileName, ProfilePictureByteData, ProfilePictureMIMEType FROM profile_info)
+        if entry[0] == user_id:
+            db_cursor.close()
+            connection_to_db.close()
+            return send_file(path_or_file=io.BytesIO(entry[2]), mimetype=entry[3], as_attachment=False)
+    #------------------------------------------------------
+
+    #--user_id is not valid--
+    db_cursor.close()
+    connection_to_db.close()
+    return render_template("error.html", error_message = "Uh oh! The linked you visited is not valid.\nDouble check that you're using the right link.") # returns an error page to the user
+    #------------------------
+
 """    
 """
 """
