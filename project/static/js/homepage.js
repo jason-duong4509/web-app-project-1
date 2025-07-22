fed1abf0f9dc7cf8cccbcc32c4b42c02c9408a00
jason-duong4509 <jasonduong09@gmail.com>
2025-07-21 19:23:24 -0600

    Syntax error fix #11

--- project/static/js/homepage.js
+++ project/static/js/homepage.js
@@ -1,27 +1,24 @@
-document.getElementById("logout_button").addEventListener("click", async event =>{
-    event.preventDefault(); 
-    const request = await fetch("/logout", method = POST); //Call backend to log the user out
+document.getElementById("options").addEventListener("change", async event =>{//Change event -> when an option is selected
+    if (event.form === "log_out"){ //Log out option was chosen
+        const request = await fetch("/logout", method = POST); //Call backend to log the user out
     
-    //--Get the result and interpret it--
-    const results = await request.json(); //Gets the results of the request to the backend
-    window.location.replace(results.url); //Change the URL to the one provided
-    //-----------------------------------
-});
+        //--Get the result and interpret it--
+        const results = await request.json(); //Gets the results of the request to the backend
+        window.location.replace(results.url); //Change the URL to the one provided
+        //-----------------------------------
+    } else if (event.form === "profile"){//View profile option was chosen
+        const request = await fetch("/p/own", method = GET); //Call backend to display the user's own profile
 
-document.getElementById("view_profile_button").addEventListener("click", async event =>{
-    event.preventDefault();
-    const request = await fetch("/p/own", method = GET); //Call backend to display the user's own profile
-
-    //--Get the result and interpret it--
-    const results = await request.json(); //Gets the results of the request to the backend
-    window.location.replace(results.url); //Change the URL to the one provided
-    //-----------------------------------
+        //--Get the result and interpret it--
+        const results = await request.json(); //Gets the results of the request to the backend
+        window.location.replace(results.url); //Change the URL to the one provided
+        //-----------------------------------
+    }
 });
 
 document.getElementById("search_bar").addEventListener("keypress", async event => {
-    event.preventDefault();
-
     if (event.key === "Enter"){ //Checks that the key pressed is Enter. === means equal in value and type (will not accept strings "Enter")
+        event.preventDefault();
         //--Input checking--
         const userInput = document.getElementById("search_bar").innerHTML;
         if (userInput.length > 100 || userInput.length <= 0 || Number.isFinite(Number(userInput)) == true){ //User input is unrealistically large/small or is a number
