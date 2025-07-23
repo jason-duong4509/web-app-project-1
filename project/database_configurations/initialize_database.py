63e787c1f85777d3406cca73601cc1118f8d587c
jason-duong4509 <jasonduong09@gmail.com>
2025-07-22 19:01:29 -0600

    fix

--- project/database_configurations/initialize_database.py
+++ project/database_configurations/initialize_database.py
@@ -7,7 +7,7 @@ Author: Jason Duong
 import psycopg2 # Needed to connect to the database
 
 #--Set up and connect to DB--
-DATABASE_URL = "INSERT DATABASE URL HERE" # Placeholder for when running the file locally during database set up
+DATABASE_URL = "INSERT DATABASE URL" # Placeholder for when running the file locally during database set up
 connection_to_db = psycopg2.connect(DATABASE_URL)
 database_cursor = connection_to_db.cursor()
 #----------------------------
@@ -20,6 +20,16 @@ database_cursor.execute("SELECT * FROM user_info")
 print(database_cursor.fetchall())
 database_cursor.execute("INSERT INTO profile_info (Bio, ProfilePictureFileName, ProfilePictureByteData, ProfilePictureMIMEType, Attachment1FileName, Attachment1ByteData, Attachment1MIMEType, Attachment2FileName, Attachment2ByteData, Attachment2MIMEType, Attachment3FileName, Attachment3ByteData, Attachment3MIMEType) VALUES (NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL)") # Insert test data
 database_cursor.execute("SELECT * FROM profile_info")
+print(database_cursor.fetchall())
+
+#----Prepare the default data that will be inserted into the DB----
+default_pfp_name = "default_image.png"
+default_pfp_byte_data = open("default_image.png", "rb").read() # Open the image in read byte mode and read its contents
+default_pfp_mime_type = "image/png" # The MIME type of the image
+#------------------------------------------------------------------
+
+database_cursor.execute("INSERT INTO default_data (PFP_File_Name, PFP_Byte_Data, PFP_MIME_Type) VALUES (%s, %s, %s)", (default_pfp_name, default_pfp_byte_data, default_pfp_mime_type))
+database_cursor.execute("SELECT * FROM default_data")
 print(database_cursor.fetchall())
 #-------------------------------------------------------------------------
 
