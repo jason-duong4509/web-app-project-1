78910de047d1c2cbe4384e2a7f6bbf50692cba01
jason-duong4509 <jasonduong09@gmail.com>
2025-07-22 19:16:53 -0600

    fix

--- project/backend.py
+++ project/backend.py
@@ -336,6 +336,7 @@ def getProfilePicture(user_id): # user_id = the user id in the URL when the requ
     connection_to_db.close()
     for entry in results_table: # entry = (UserID, ProfilePictureFileName, ProfilePictureByteData, ProfilePictureMIMEType)
         if entry[0] == user_id: # Found the desired user
+            print("PROFILE PICTURE LOADED")
             return send_file(path_or_file=io.BytesIO(bytes(entry[2])), mimetype=entry[3], as_attachment=False)
     #------------------------------------------------------
 
