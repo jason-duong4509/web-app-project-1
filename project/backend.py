d5b7bdb8e2cc83a5828be9bb282c94712db52127
jason-duong4509 <jasonduong09@gmail.com>
2025-07-25 12:58:01 -0600

    hh

--- project/backend.py
+++ project/backend.py
@@ -440,14 +440,11 @@ def changePFP(user_id):
         user_id = int(user_id)
         user_is_editing_someones_profile = user_id != int(current_user.id) # UserID the user is editing is not their own
 
-        print(f"ARE YOU EDITING SOMEONE ELSES PROFILE {user_is_editing_someones_profile}")
-
         new_pfp = request.files["newPFP"] # Gets the file sent from the user (contents are in binary)
         file_mime_type = magic.from_buffer(new_pfp.read(2048), mime=True) # Reads the first 2048 bytes (recommended amount) of the file and guess the MIME type
         mime_type_is_incorrect = not (file_mime_type == "image/png") # Checks if the file's MIME type is a PNG
         new_pfp.seek(0) # Move the pointer back to the beginning of the file
-
-        print(f"IS THE MIME TYPE INCORRECT {mime_type_is_incorrect}")
+        new_pfp_bytes = new_pfp.read() # Read the file (in bytes) and store it
 
         #TODO: add file size check (too big = reject)
 
@@ -463,14 +460,14 @@ def changePFP(user_id):
 
     #--Update profile info for the user in the DB--
     new_pfp_file_name = "%i_profile_picture.png" % user_id
-    db_cursor.execute("UPDATE profile_info SET ProfilePictureFileName = %s, ProfilePictureByteData = %s, ProfilePictureMIMEType = %s WHERE UserID = %i", (new_pfp_file_name, new_pfp, "image/png", user_id))
+    db_cursor.execute("UPDATE profile_info SET ProfilePictureFileName = %s, ProfilePictureByteData = %s, ProfilePictureMIMEType = %s WHERE UserID = %i", (new_pfp_file_name, psycopg2.Binary(new_pfp_bytes), "image/png", user_id))
     db_cursor.commit()
     #----------------------------------------------
 
     db_cursor.close()
     connection_to_db.close()
 
-    return send_file(path_or_file=io.BytesIO(new_pfp), mimetype="image/png", as_attachment=False) # Send the new PFP back to the front end so it can display it to the user
+    return send_file(path_or_file=io.BytesIO(new_pfp_bytes), mimetype="image/png", as_attachment=False) # Send the new PFP back to the front end so it can display it to the user
 
 """
 Function that attempts to change one of the user's given attachments
