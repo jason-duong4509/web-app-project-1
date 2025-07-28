25c275e583c35763c62b8824e902089b6dd7bf3e
jason-duong4509 <jasonduong09@gmail.com>
2025-07-28 10:58:02 -0600

    w

--- project/backend.py
+++ project/backend.py
@@ -253,7 +253,7 @@ def deleteAccount(user_id):
         if found_user and username_matches and password_matches: # User entered the correct information
             db_cursor.execute("DELETE FROM user_info WHERE UserID = %s", (user_id,)) # Delete the user from this table
             db_cursor.execute("DELETE FROM profile_info WHERE UserID = %s", (user_id,)) # Delete the user from this table
-            db_cursor.commit()
+            connection_to_db.commit()
             db_cursor.close()
             connection_to_db.close()
             logout_user() # Logs the user out
