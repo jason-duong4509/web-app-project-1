4cb819494fc96056465fcd8250aea96b29a564b7
jason-duong4509 <jasonduong09@gmail.com>
2025-09-20 15:17:07 -0600

    changes to fix errors

--- project/backend/backend.py
+++ project/backend/backend.py
@@ -757,6 +757,7 @@ def onViewProfile(user_id): # Takes whatever is after "/p/" and passes it as a p
         return render_template("error.html", error_message = "Uh oh! The linked you visited is not valid. Double check that you're using the right link.") # returns an error page to the user
     #--------------------------------
 
+    print("SERVED INDEX.HTML")
     return send_from_directory("../frontend/dist/", "index.html") # Grab the react file and serve it
 
 """
