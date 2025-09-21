6667b03ce6a1877ceac6f085489c4c25f2b5c1dc
jason-duong4509 <jasonduong09@gmail.com>
2025-09-20 19:30:51 -0600

    changes to fix errors

--- project/frontend/vite.config.js
+++ project/frontend/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "dist/", //Change the pathname for react so it can grab the right files
+  base: "static/dist/", //Change the pathname for react so it can grab the right files
 })
