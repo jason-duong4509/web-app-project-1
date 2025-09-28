bcd616524690d3acbc9d82632c2f33613869f0e2
jason-duong4509 <jasonduong09@gmail.com>
2025-09-27 22:50:22 -0600

    changes to fix errors

--- project/backend/backend.py
+++ project/backend/backend.py
@@ -711,9 +711,9 @@ def getInitialProfileData(user_id):
     #-----------------------------------
 
     #--Prepare user data to send back to front-end--
-    editing_disabled = current_user.id != user_id # Editing is disabled if the userID belonging to the viewing profile is not the current user's ID
+    editing_disabled = int(current_user.id) != user_id # Editing is disabled if the userID belonging to the viewing profile is not the current user's ID
     print(f"BACKEND: CURRENT USER ID: {current_user.id} USER ID OF PROFILE: {user_id} IS EDITING DISABLED {editing_disabled}")
-    
+
     return jsonify({ # Return all of the needed user data in the form of JSON to the front end
         "userID" : user_id,
         "currentUserID" : current_user.id,
