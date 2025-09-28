72b023a10b8f6adf0f37cb4f2df68b66ff9e5fce
jason-duong4509 <jasonduong09@gmail.com>
2025-09-27 22:38:11 -0600

    changes to fix errors

--- project/backend/backend.py
+++ project/backend/backend.py
@@ -711,6 +711,7 @@ def getInitialProfileData(user_id):
     #-----------------------------------
 
     #--Prepare user data to send back to front-end--
+    print(f"BACKEND: CURRENT USER ID: {current_user.id} USER ID OF PROFILE: {user_id} IS EDITING DISABLED {editing_disabled}")
     editing_disabled = current_user.id != user_id # Editing is disabled if the userID belonging to the viewing profile is not the current user's ID
 
     return jsonify({ # Return all of the needed user data in the form of JSON to the front end
--- project/frontend/src/js/profile.js
+++ project/frontend/src/js/profile.js
@@ -261,9 +261,7 @@ export async function changeProfilePicture(returnFunction){
                 newUserDataObject.updateProfileErrorMessage = "NONE"; //Reset the error message
             }
         }
-        console.log("is editing profile" + newUserDataObject.isEditingProfile);
         copyUserdata(newUserDataObject);
-        console.log("is editing profile" + newUserDataObject.isEditingProfile);
         returnFunction(newUserDataObject); //Call the given react function to update state 
     };
 }
@@ -487,7 +485,6 @@ export async function submitProfileInfo(event, returnFunction){
         if (results.success === false){ //Form was rejected by backend
             newUserDataObject.updateProfileErrorMessage = "ERROR_MESSAGE";
         } else if (results.success === true){//Form was accepted by backend
-            console.log("CALLED THIS FUNCTION FOR SOME REASON")
             newUserDataObject.isEditingProfile = false;
 
             //--Updates each profile attribute to reflect the changes the user made--
